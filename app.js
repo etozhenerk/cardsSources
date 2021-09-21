e4cc70db7d36ad07c1e944ba38bcb80e2c64b413
Mikhail <mmalahov@team.amocrm.com>
2021-09-21 09:27:58 +0300

    change app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 "use strict";
-const slides = document.querySelectorAll(".slide");
-const container = document.querySelector(".container");
+const slidesPlugin = (acrive = 0) => {
+  const slides = document.querySelectorAll(".slide");
+  const container = document.querySelector(".container");
 
-const clearActiveClasses = () => {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
+  slides[acrive].classList.add('active');
+
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  };
+
+  container.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.closest(".slide")) {
+      clearActiveClasses();
+      target.classList.add("active");
+    }
   });
 };
 
-container.addEventListener("click", (e) => {
-  const target = e.target;
-  if (target.closest(".slide")) {
-    clearActiveClasses();
-    target.classList.add("active");
-  }
-});
+slidesPlugin(0);
